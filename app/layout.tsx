d347633cf55fd8480ab5e3ba42128e99917375b5
Eduardo Pailemilla <edu.pailemilla@gmail.com>
2025-11-09 00:29:44 -0300

    FIX: logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,23 +10,23 @@ export const metadata: Metadata = {
   title: 'Dark Frame Motors',
   description: 'Dark Frame Motors',
   generator: 'Dark Frame Motors',
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
+  // icons: {
+  //   icon: [
+  //     {
+  //       url: '/icon-light-32x32.png',
+  //       media: '(prefers-color-scheme: light)',
+  //     },
+  //     {
+  //       url: '/icon-dark-32x32.png',
+  //       media: '(prefers-color-scheme: dark)',
+  //     },
+  //     {
+  //       url: '/icon.svg',
+  //       type: 'image/svg+xml',
+  //     },
+  //   ],
+  //   apple: '/apple-icon.png',
+  // },
 }
 
 export default function RootLayout({
